890637586ad70dbf630c99c8a77e2e78d4d8ae99
2017zhaoxiaoxiao <1877145940@qq.com>
2020-04-12 16:29:52 +0800

    添加课件以及课上练习

---
+++ 练习代码/js/we.js
@@ -0,0 +1,13 @@
+// alert('不让你打开');
+// alert('你怎么这么不听话');
+// console.log('he');
+// document.write('<table>');
+// function add(x,y){
+		// 	var sum=0;
+		// 	for(var num1=x;x<=y;num1++){
+		// 		sum+=num1;
+				
+		// 	}
+		// 	return sum;
+		// }
+		// alert(add(1,3));
